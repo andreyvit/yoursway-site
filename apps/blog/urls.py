549870da773098238911c46343afe59e435f2e0c
Yuri Baburov <burchik@gmail.com>
2008-04-15 14:56:34 +0700

    Reversed posts order on the blog main page.

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,5 +3,5 @@ from django.views.generic.list_detail import object_list
 from apps.blog.models import Post
 
 urlpatterns = patterns('',
-      (r'^$', object_list, {'queryset':Post.objects.all()}),
+      (r'^$', object_list, {'queryset':Post.objects.all().order_by('-pub_date')}),
 )
---
+++ pull_db.sh
@@ -0,0 +1,1 @@
+scp yoursway.com:/var/django/yoursway/blog.db .
